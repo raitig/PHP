76551342d513836c12f114f84fb2a13af0211c0e
Raitig <166475951+raitig@users.noreply.github.com>
2025-02-13 00:31:34 +0530

    Add files via upload

---
+++ Assignment 2/oddeven.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Even or checker </title>
+</head>
+<body>
+    <form action="oddevencheck.php" method="post">
+        <label> Enter an integer : </label> &nbsp;&nbsp;
+        <input type="Number" name="Number" required>
+        <br><br>
+        <input type = "Reset">&nbsp;&nbsp;
+        <input type ="Submit" value="Submit">
+</form>
+</body>
+</html>
---
+++ Assignment 2/oddevencheck.php
@@ -0,0 +1,7 @@
+<?php
+$n=$_POST["Number"];
+if($n%2==0)
+echo $n."is even";
+else
+echo $n."is odd";
+?>
