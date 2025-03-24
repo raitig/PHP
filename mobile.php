9c2d38948553b050733df9d369d7230a17d6ed3c
Raitig sarkar <166475951+raitig@users.noreply.github.com>
2025-03-24 13:33:07 +0530

    Delete mobile.php

--- mobile.php
+++
@@ -1,9 +0,0 @@
-<!-- Write a php program to detect remote device is mobibe device or not -->
- <?php
- if(preg_match('/Mobile|Android|iphone|ipad|ipod|Black berry|windows phone/',$_SERVER['HTTP_USER_AGENT'])){
-    echo " This ia a mobile device...........";
- }
- else{
-    echo "This is not a mobile device.........";
- }
- ?>
