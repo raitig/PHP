27d0330bb7364cef5f7cd11682a9b9bfafc53575
Raitig sarkar <166475951+raitig@users.noreply.github.com>
2025-04-14 14:47:57 +0530

    Add files via upload

---
+++ Assignment15/create header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> My website </title>
+    <style>
+        body{font-family:Arial, Helvetica, sans-serif;margin: 0; padding: 0;}
+        .navbar{background-color: #333;padding:10px;}
+        .navbar a{
+            color: white;
+            text-decoration: none;
+            margin-right: 20px;
+            font-weight: bold;
+        }
+        .navbar a:hover{
+            text-decoration: underline;
+        }
+        .content{padding: 20px;}
+    </style>
+</head>
+<body>
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+    </div>
+    <div class="content">
---
+++ Assignment15/index.php
@@ -0,0 +1,7 @@
+<?php
+include "create header.php";?>
+<h1> Welcome to my home page </h1>
+<p> This is home page content. Here you can add more content to design your page </p>
+</div>
+</body>
+</html>
